f4353d76674a0b55843e62d86b2f30b86c1661e2
Vinsmoke-Tech <daffahanaris@gmail.com>
2025-04-09 20:43:23 +0700

    update(API Route) Change Route Index

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 
 Route::prefix('products')->controller(ProductController::class)->group(function () {
-    Route::get('/list', 'index');
+    Route::get('/', 'index');
     Route::get('{product}', 'show');
     Route::post('/', 'store');
     Route::put('{product}', 'update');
